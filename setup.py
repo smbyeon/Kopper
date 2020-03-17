d24e46e68d4a881fe0f4f8dab919627d4588c08e
Seongmok Byeon <smbyun0214@gmail.com>
2020-03-17 13:15:23 +0900

    version 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
  
 setuptools.setup(
     name="kopper",
-    version="0.1.3",
+    version="0.1.1",
     author="Seongmok Byeon",
     author_email="smbyun0214@gmail.com",
     description="This is a Korail seat reservation support service. \
